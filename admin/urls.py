f1a67e5bef368cea7c51691a02c4431c3cd88571
juhwano <calculus9006@gmail.com>
2022-02-09 21:16:03 +0900

    ✨관리자 설문 생성, 상세 페이지

--- admin/urls.py
+++ admin/urls.py
@@ -4,17 +4,20 @@ from . import views
 app_name ='admin'
 
 urlpatterns = [
+    # 관리자 메인 페이지
     path('', views.index, name='index'),
-    # 설문지 목록 보기(admin)
+    # 설문지 목록 보기
     path('polls/', views.poll_list, name='list'),
-    # 설문지 상세 보기(admin/5)
+    # 설문지 상세 보기
     path('polls/<int:pk>', views.poll_detail, name='detail'),
-    # 설문지 생성(admin/create)
+    # 설문지 생성 페이지
     path('polls/create/', views.poll_create, name='create'),
-    # 설문지 수정(admin/5/update)
+    # 설문지 생성 데이터 저장
+    path('polls/new/', views.new_poll, name='new'),
+    # 설문지 수정
     path('polls/<int:pk>/update', views.poll_update, name='update'),
-    # 설문지 삭제(admin/delete)
+    # 설문지 삭제
     path('polls/<int:pk>/delete', views.poll_delete, name='delete'),
-    # 설문지 분석(admin/a)
+    # 관리자 분석 페이지
     path('a/', views.poll_analysis, name='analysis'),
 ]
--- admin/views.py
+++ admin/views.py
@@ -8,25 +8,43 @@ from django.views import View
 def index(request):
     return render(request, 'admin/index.html')
 
+# 설문지 목록 최신순 출력
 def poll_list(request):
-    # 진행중인 설문지 목록 최신순 출력
     polls = Question.objects.all().order_by('-created_at')
     context = {
         'polls' : polls
     }
     return render(request, 'admin/poll_list.html', context)
 
-def poll_detail(request):
-    pass
+# 특정 설문지 하나만 출력
+def poll_detail(request, pk):
+    poll = Question.objects.get(id=pk)
+    choice = Choice.objects.filter(question_id=pk)
+    context = {
+        'poll' : poll,
+        'choice' : choice,
+    }
+    return render(request, 'admin/poll_detail.html', context)
 
+# 설문지 생성 페이지로 이동
 def poll_create(request):
-    # 설문지 생성
-    if request.method == 'POST':
-        poll_data = request.POST
+    return render(request, 'admin/poll_create.html')
 
-        data = Question(question_text= poll_data.get('poll_name'), choice_count=poll_data.get('choice_count'), status=poll_data.get('status'), created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
-        data.save()
-        return redirect('/admin/')
+# 설문지 생성 DB 작업
+def new_poll(request):
+    if request.method == 'POST':
+        question_name = request.POST.get('question_name')
+        question_count = request.POST.get('question_count')
+        question_status = request.POST.get('question_status')
+        choice_name = request.POST.getlist('choice_name')
+        print(choice_name)
+
+        question_data = Question(question_text= question_name, count=question_count, status=question_status, created_at=datetime.datetime.now(), updated_at=datetime.datetime.now())
+        choice_data = Choice(choice_text=choice_name) # id는 어떻게?
+
+        question_data.save()
+        choice_data.save()
+        return redirect('/admin/polls')
 
 def poll_update(request, pk):
     if request.method == 'POST':
